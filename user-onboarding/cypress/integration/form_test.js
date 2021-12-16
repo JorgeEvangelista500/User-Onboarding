5b6de26085a6b4c21c7c4c9559ef47bc6c5c650b
Jorge Evangelista <jorgeevangelista500@gmail.com>
2021-12-16 16:33:41 -0500

    finished the test

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -7,7 +7,7 @@ describe('Member App', () => {
     const passwordInput = () => cy.get('input[name=password]');
     const emailInput = () => cy.get('input[name=email]');
     const submitBtn = () => cy.get('button[id="submitBtn"]');
-    // const tos = () => cy.get('[type="checkbox"]')
+    const tos = () => cy.get('[type="checkbox"]')
 
     it('sanity check!', () => {
         expect(2 + 2).to.equal(4);
@@ -21,10 +21,10 @@ describe('Member App', () => {
         passwordInput().should('exist');
         emailInput().should('exist');
         submitBtn().should('exist');
-        // tos().should('exist');
+        tos().should('exist');
     })
 
-    describe('Filling out the inputs and submit', () => {
+    describe('Filling out the inputs', () => {
 
         it('submit button starts out disabled',() => {
             submitBtn().should('be.disabled')
@@ -48,12 +48,30 @@ describe('Member App', () => {
            .should('have.value','wasd')
         })
 
+        it('the Terms of service checkbox works',() => {
+            tos().check()
+            tos().should('be.checked')
+        })
+
         it('the submit button enables when all fields are filled', () => {
             nameInput().type('Elvis')
             emailInput().type('elvis@evan.com')
             passwordInput().type('dsaw')
-            cy.get('[type="checkbox"]').check()
+            tos().check()
             submitBtn().should('not.be.disabled')
+        })
+    })
+
+    describe('Adding a new member', () => {
+        it('can submit a new member', () => {
+            nameInput().type('Gustavo')
+            emailInput().type('gustavo@evan.com')
+            passwordInput().type('abc')
+            tos().check()
+            submitBtn().click()
+            cy.contains('Gustavo').should('exist')
+            // cy.contains('Gustavo').next().click()
+            // cy.contains('Gustavo').should('not.exist') dont have a delete button
         })
     })
 
